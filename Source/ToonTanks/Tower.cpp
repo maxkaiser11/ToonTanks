01ebe29d3ee60cc052581a8313d587e8e2fe0dcf
Max Kaiser <max@kaiser.family>
2025-07-08 12:51:09 +0200

    Timers

--- Source/ToonTanks/Tower.cpp
+++ Source/ToonTanks/Tower.cpp
@@ -4,24 +4,14 @@
 #include "Tower.h"
 #include "Tank.h"
 #include "Kismet/GameplayStatics.h"
+#include "TimerManager.h"
 
 void ATower::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
-
-	// Find Distance to the Tank
-	if (IsValid(Tank))
-	{
-		float Distance = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
-		
-		// Check to see if the Tank is in range
-		if (Distance <= FireRange)
-		{
-			// If in range, rotate turret toward tank
+	
+		if (InFireRange())
 			RotateTurret(Tank->GetActorLocation());
-		}
-		
-	}
 	
 }
 
@@ -30,5 +20,24 @@ void ATower::BeginPlay()
 	Super::BeginPlay();
 
 	Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
-	
+
+
+	GetWorldTimerManager().SetTimer(FireRateTimerHandle, this, &ATower::CheckFireCondition, FireRate, true);
+}
+
+void ATower::CheckFireCondition()
+{
+		if (InFireRange())
+			Fire();
+}
+
+bool ATower::InFireRange()
+{
+	if (IsValid(Tank))
+	{
+		float Distance = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
+		if (Distance <= FireRange)
+			return true;
+	}
+	return false;
 }
